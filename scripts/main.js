8609454afacd1ca189ac65c174bc4e2ab03a14a8
Walter van den Houten <waltervdhouten@gmail.com>
2020-12-10 22:35:47 +0200

    test anchor script

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const anchors = document.querySelectorAll('a');
-    anchors.forEach(a => {
-        if (a.href.startsWith('#')) {
-            console.log(a);
-        }
-    });
+const anchors = document.querySelectorAll('a');
+anchors.forEach(a => {
+    if (a.href.startsWith('#')) {
+        console.log(a);
+    }
 });
