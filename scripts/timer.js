ef053b0bbc78675f04862a5871093a5fc4e4cceb
Walter van den Houten <waltervdhouten@gmail.com>
2020-12-28 16:05:10 +0200

    -

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,7 +1,7 @@
 // TODO: Add pause/resume functionality.
 class Timer {
     __timer;
-    
+
     clear() {
         if (this.__timer)
             window.clearInterval(this.__timer);
@@ -15,16 +15,16 @@ class Timer {
         const timerElement = document.createElement('div');
         timerElement.id = 'timer';
         document.body.appendChild(timerElement)
-    
+
         duration = this.__parseDuration(duration);
         const start = Date.now();
-        __timer = setInterval(() => {
+        this.__timer = setInterval(() => {
             const delta = Date.now() - start;
             const secondsRemaining = duration - delta / 1000;
             const timeRemaining = new Date(secondsRemaining * 1000).toISOString().substr(11, 8)
-    
+
             timerElement.innerText = timeRemaining;
-    
+
             if (secondsRemaining <= 0)
                 this.clear();
         }, 100);
@@ -41,12 +41,12 @@ class Timer {
         const match = duration.match(/((\d+)h)?((\d+)m)?((\d+)s?)?/gi);
         duration = 0;
         if (match[1])
-            duration += parseInt(match[2]) * 3600; 
+            duration += parseInt(match[2]) * 3600;
         if (match[3])
-            duration += parseInt(match[4]) * 60; 
+            duration += parseInt(match[4]) * 60;
         if (match[5])
             duration += parseInt(match[6]);
-        return duration; 
+        return duration;
     }
 }
 
